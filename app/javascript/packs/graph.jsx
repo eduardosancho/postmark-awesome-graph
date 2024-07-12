5f8edc1be2fdd454df253976e49f7eaf72285983
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2024-07-11 21:01:14 -0600

    Feat: Display topics accordingly

--- app/javascript/packs/graph.jsx
+++ app/javascript/packs/graph.jsx
@@ -56,8 +56,7 @@ Inspector.propTypes = {
 const App = ({ snapshot }) => {
   const [currentSource, setCurrentSource] = React.useState()
   const [currentTarget, setCurrentTarget] = React.useState()
-
-  const topics = 'Cheese and Wine' // FIXME
+  const [currentTopics, setCurrentTopics] = React.useState()
 
   const handleClickNode = _nodeId => {}
 
@@ -70,11 +69,13 @@ const App = ({ snapshot }) => {
   const handleMouseOverLink = (source, target) => {
     setCurrentSource(source)
     setCurrentTarget(target)
+    setCurrentTopics(snapshot.links.find(l => l.source === source && l.target === target).topics);
   }
 
   const handleMouseOutLink = (_source, _target) => {
     setCurrentSource(undefined)
     setCurrentTarget(undefined)
+    setCurrentTopics(undefined)
   }
 
   return (
@@ -83,7 +84,7 @@ const App = ({ snapshot }) => {
         <Inspector
           source={currentSource}
           target={currentTarget}
-          topics={topics}
+          topics={currentTopics}
         />
       </div>
       <Graph
@@ -107,8 +108,9 @@ App.propTypes = {
 const DUMMY_SNAPSHOT = {
   nodes: [{ id: 'Harry' }, { id: 'Sally' }, { id: 'Alice' }],
   links: [
-    { source: 'Harry', target: 'Sally' },
-    { source: 'Harry', target: 'Alice' },
+    { source: 'Harry', target: 'Sally', topics: 'Topic1 and Topic5' },
+    { source: 'Harry', target: 'Alice', topics: 'Topic2 and Topic6' },
+    { source: 'Sally', target: 'Alice', topics: 'Topic3 and Topic7' },
   ],
 }
 
